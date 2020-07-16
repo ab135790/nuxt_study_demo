fc229ce39190a7fe7d40f73fb593d74f12b6bb21
WJN06 <275284429@qq.com>
2020-07-16 22:28:09 +0800

    2020-7-16

---
+++ components/Navbar.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <nuxt-link class="link" to="/create">create Link</nuxt-link>
+    <nuxt-link class="link" to="/about">about Link</nuxt-link>
+    <nuxt-link class="link" to="/list">list Link</nuxt-link>
+  </div>
+</template>
+<script>
+export default {}
+</script>
+<style>
+.link {
+  margin-right: 10px;
+}
+</style>
